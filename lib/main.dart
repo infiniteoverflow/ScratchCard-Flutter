17b86a1beb68e18b1d41cf8f5a5f87c092f82f91
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-02-28 14:16:35 +0530

    Added App files

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:scratcher/scratcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Scratcher(
+          brushSize: 40,
+          threshold: 50,
+          // color: Colors.red,
+          image: Image.asset('assets/scratchBanner.png'),
+          onChange: (value) {},
+          onThreshold: () => print("Threshold reached, you won!"),
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/prize.png')
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
