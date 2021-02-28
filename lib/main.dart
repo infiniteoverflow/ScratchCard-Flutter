59ba6dfa8f9cdbbcb77f762529dfce323e2a7f0d
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-02-28 14:22:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
